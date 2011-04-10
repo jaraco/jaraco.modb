70bdf0072a09b7edb2921770e84ee4c394943ef2
Jason R. Coombs <jaraco@jaraco.com>
2011-04-10 13:49:38 -0400

    Added support for binary data when encoding to bson

--- jaraco/modb/__init__.py
+++ jaraco/modb/__init__.py
@@ -1,11 +1,32 @@
 import jsonpickle
+import pymongo.binary
+import jaraco.util.dictlib
+from jaraco.util.string import is_binary
 
 # set up a couple of "serialization" functions for jsonpickle to produce
-#  and consume JSON-ready dicts, rather than serializing to strings.
-dumps = loads = lambda x: x
+#  and consume BSON-ready dicts, rather than serializing to strings.
+def to_bson(json):
+	# all this just so we can convert 'bytes' objects to pymongo Binary
+	if isinstance(json, dict):
+		return jaraco.util.dictlib.dict_map(to_bson, json)
+	if isinstance(json, list):
+		return map(to_bson, json)
+	if is_binary(json):
+		return pymongo.binary.Binary(json)
+	return json
+
+def from_bson(json):
+	# all this just so we can convert pymongo Binary back to 'bytes'
+	if isinstance(json, dict):
+		return jaraco.util.dictlib.dict_map(from_bson, json)
+	if isinstance(json, list):
+		return map(from_bson, json)
+	if isinstance(json, pymongo.binary.Binary):
+		return bytes(json)
+	return json
 
 def init():
-	jsonpickle.load_backend(__name__, 'dumps', 'loads', ValueError)
+	jsonpickle.load_backend(__name__, 'to_bson', 'from_bson', ValueError)
 	jsonpickle.set_preferred_backend(__name__)
 
 encode = jsonpickle.encode
