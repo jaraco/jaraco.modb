54acbe94235e3caaf64b2877aaff228829e93db4
Jason R. Coombs <jaraco@jaraco.com>
2013-04-17 15:54:07 -0400

    Added test for OrderedDict in jaraco.modb

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 import bson.binary
+import py.test
+
 import jaraco.modb
 
 def test_to_bson():
@@ -46,3 +48,15 @@ def test_encode_dict_subclass():
 def test_init():
 	"Init should do nothing, not cause errors"
 	jaraco.modb.init()
+
+def test_ordered_dict():
+	try:
+		import collections
+		ob = collections.OrderedDict(a=1, c=3, b=4)
+	except Exception:
+		py.test.skip("OrderedDict not available")
+	serialized = jaraco.modb.encode(ob)
+	restored = jaraco.modb.decode(serialized)
+	assert isinstance(restored, collections.OrderedDict)
+	assert restored.keys() == list('acb')
+	assert restored.values() == [1, 3, 4]
