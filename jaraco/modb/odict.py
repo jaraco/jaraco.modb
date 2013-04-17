a49cab69ba7d77be6922f2026c7df3fdd072bb90
Jason R. Coombs <jaraco@jaraco.com>
2013-04-17 11:18:35 -0400

    Restore Python 2.6 compatibility (without OrderedDict support)

--- jaraco/modb/odict.py
+++ jaraco/modb/odict.py
@@ -1,7 +1,9 @@
+import sys
 import collections
 
 from . import handlers
 
 @handlers.register
 class OrderedDictHandler(handlers.SimpleReduceHandler):
-    _handles = collections.OrderedDict,
+    if sys.version_info > (2,7):
+        _handles = collections.OrderedDict,
