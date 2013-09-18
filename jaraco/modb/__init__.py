249d4a749afc72211942622610ad96e90cd3c57e
Gustavo Picon <tabo@tabo.pe>
2013-09-18 14:13:57 -0400

    work with jsonpickle 0.6

--- jaraco/modb/__init__.py
+++ jaraco/modb/__init__.py
@@ -6,21 +6,19 @@ import jsonpickle.pickler
 import jsonpickle.unpickler
 import bson.binary
 from jaraco.util.string import is_binary
-
 # override the default pickler/unpickler to handle binary strings
 class Pickler(jsonpickle.pickler.Pickler):
-	def flatten(self, obj):
+	def flatten(self, obj, reset=True):
 		if isinstance(obj, dt.datetime) and not obj.utcoffset():
 			# naive datetimes or UTC datetimes can be stored directly
 			return obj
-		flattened = super(Pickler, self).flatten(obj)
+		flattened = super(Pickler, self).flatten(obj, reset)
 		if is_binary(flattened):
 			return bson.binary.Binary(flattened)
 		return flattened
-
 class Unpickler(jsonpickle.unpickler.Unpickler):
-	def restore(self, obj):
-		restored = super(Unpickler, self).restore(obj)
+	def restore(self, obj, reset=True):
+		restored = super(Unpickler, self).restore(obj, reset)
 		if isinstance(restored, bson.binary.Binary):
 			return bytes(restored)
 		return restored
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup_params = dict(
 		"Programming Language :: Python :: 3",
 	],
 	install_requires=[
-		'jsonpickle>=0.5.0-rc1',
+		'jsonpickle>=0.6',
 		'pymongo',
 		'jaraco.util',
 	],
