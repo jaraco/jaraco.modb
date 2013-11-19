c8bd50209d819a2ea61c564d1c428c2131268b2f
Jason R. Coombs <jaraco@jaraco.com>
2013-11-18 20:46:45 -0500

    Remove odict module

--- jaraco/modb/odict.py
+++
@@ -1,9 +0,0 @@
-import sys
-import collections
-
-from . import handlers
-
-@handlers.register
-class OrderedDictHandler(handlers.SimpleReduceHandler):
-    if sys.version_info > (2,7):
-        _handles = collections.OrderedDict,
