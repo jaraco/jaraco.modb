872c542c9f1e2da72eed42018e4587f9fe8f14c3
Jason R. Coombs <jaraco@jaraco.com>
2013-11-18 20:26:04 -0500

    Override Pickler._flatten and Unpickler._restore as that's what the underlying methods call.

--- jaraco/modb/__init__.py
+++ jaraco/modb/__init__.py
@@ -9,18 +9,18 @@ from jaraco.util.string import is_binary
 
 # override the default pickler/unpickler to handle binary strings
 class Pickler(jsonpickle.pickler.Pickler):
-	def flatten(self, obj, reset=True):
+	def _flatten(self, obj):
 		if isinstance(obj, dt.datetime) and not obj.utcoffset():
 			# naive datetimes or UTC datetimes can be stored directly
 			return obj
-		flattened = super(Pickler, self).flatten(obj, reset)
+		flattened = super(Pickler, self)._flatten(obj)
 		if is_binary(flattened):
 			return bson.binary.Binary(flattened)
 		return flattened
 
 class Unpickler(jsonpickle.unpickler.Unpickler):
-	def restore(self, obj, reset=True):
-		restored = super(Unpickler, self).restore(obj, reset)
+	def _restore(self, obj):
+		restored = super(Unpickler, self)._restore(obj)
 		if isinstance(restored, bson.binary.Binary):
 			return bytes(restored)
 		return restored
