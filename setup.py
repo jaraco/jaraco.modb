a8fc9bb8f907cd4897b0d03dd9cb44c1e32e40b5
Jason R. Coombs <jaraco@jaraco.com>
2018-05-11 07:54:17 -0400

    Pin jsonpickle to older version. Ref jsonpickle/jsonpickle#200.

--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ params = dict(
 	),
 	python_requires='>=2.7',
 	install_requires=[
-		'jsonpickle>=0.9.2,<1dev',
+		# pin upper bound on jsonpickle due to
+		# https://github.com/jsonpickle/jsonpickle/issues/200
+		'jsonpickle>=0.9.2,<0.9.5',
 		'pymongo',
 		'six',
 	],
